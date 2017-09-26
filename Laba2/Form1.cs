4d925903ec79087661b8c26dd190c63186415667
Digori <stasian954@mail.ru>
2017-09-26 21:48:51 +0300

    вроде как рабочее 1е задание . надо бы доработать!

--- Laba2/Form1.cs
+++ Laba2/Form1.cs
@@ -12,9 +12,52 @@ namespace Laba2
 {
     public partial class Form1 : Form
     {
+        byte F = 1;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (F == 1)
+            {
+                label1.Top = 0;
+                label1.Left += 1;
+                if (label1.Left >= ClientRectangle.Width-label1.Width)
+                {
+                    F = 2;
+                }
+            }
+            else if (F == 2)
+            {
+                label1.Left = ClientRectangle.Width-label1.Width;
+                label1.Top += 1;
+                    if (label1.Top >= ClientRectangle.Height-label1.Height)
+                {
+                    F = 3;
+                }
+            }
+            else if (F == 3)
+            {
+                label1.Top = ClientRectangle.Height-label1.Height;
+                label1.Left -= 1;
+                if(label1.Left == 0)
+                {
+                    F = 4;
+                }
+            }
+            else if (F == 4)
+            {
+                label1.Left = 0;
+                label1.Top -= 1;
+                if (label1.Top == 0)
+                {
+                    F = 1;
+                }
+            }
+
         }
     }
 }
